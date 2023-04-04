df5ccdf61cb085a6e58d0fc3f8160a5bdd93503b
Gupta0Kaustubh <kaustubhgupta9860@gmail.com>
2023-04-04 12:29:29 +0530

    3rd day

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+import bcrypt, { hash } from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
@@ -17,11 +17,50 @@ const UserSchema = new mongoose.Schema(
 
 // Attachments
 UserSchema.methods.generateJwtToken = function () { 
-    return jwt.sign({user: this._id.toString()}, "zomatoApp");
+    return jwt.sign({ user: this._id.toString() }, "zomatoApp");
 };
 
 // Helper Functions
-UserSchema.statics.findByEmailAndPhone = async () => { };
-UserSchema.statics.findByEmailAndPassword = async () => { };
+UserSchema.statics.findByEmailAndPhone = async (email, phoneNumber) => { 
+    const checkuserByEmail = await UserModel.findOne({ email });
+    const checkuserByPhone = await UserModel.findOne({ phoneNumber });
+    
+    if (checkuserByEmail || checkuserByPhone) {
+        throw new Error("User Already Exists !!!");
+    }
+    return false;
+};
+UserSchema.statics.findByEmailAndPassword = async (email, password) => {
+    const user = await UserModel.findOne({ email });
+    if (!user) throw new Error("User does not exist !!!");
+
+    // Compare Password :-
+    const doesPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!doesPasswordMatch) throw new Error("Invalid Credentials !!!");
+    
+    // If the email & password matches :-
+    return user;
+};
+ 
+UserSchema.pre("save", function (next) {
+    const user = this;
+
+    // Password is been modified or not ?
+    if (!user.isModified('password')) return next();
+
+    // Generate bcrypt and salt :-
+    bcrypt.genSalt(8, (error, salt) => {
+        if (error) return next(error);
+
+        // Hash the password 8 times :-
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if (error) return next(error);
+
+            // Will be assigning hashed password back :-
+            user.password = hash;
+            return next();
+        })
+    })
+})
 
 export const UserModel = mongoose.model("users", UserSchema);
